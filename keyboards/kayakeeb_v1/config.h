c346d95ac2db1f2f46138dfed10c57eb703e19f1
Vincent Su <vincent.su@gatech.edu>
2025-06-30 22:35:15 -0700

    enabled features, but cannot compile. Trying with windows qmk msys

--- keyboards/kayakeeb_v1/config.h
+++ keyboards/kayakeeb_v1/config.h
@@ -9,23 +9,14 @@
 // Define the specific GPIO pins on your RP2040 for communication.
 // The TX of one half must be wired to the RX of the other.
 #define SERIAL_USART_FULL_DUPLEX
-#define SERIAL_USART_TX_PIN GP8
-#define SERIAL_USART_RX_PIN GP9
+#define SERIAL_TX_PIN GP8
+#define SERIAL_RX_PIN GP9
 
 // --- Optional: Increase Communication Speed ---
 // You can optionally increase the serial communication speed for lower latency.
 // The default is generally sufficient, but for very fast typists or complex features,
 // you might consider increasing it. Default: 250000
 #define SERIAL_PIO_BAUD_RATE 250000
-
-// --- VIA/VIAL CONFIGURATION ---
-
-// 1. Assign a unique Vendor ID and Product ID for your keyboard.
-//    For a personal project, you can generate a random 32-bit number (8 hex characters)
-//    for the Product ID. The Vendor ID (VID) can be a personal, unused one.
-//    You can get one officially from pid.codes for open source projects.
-#define VENDOR_ID    0x88AA //Random VID I chose
-#define PRODUCT_ID   0xF8DE // Random PID I chose
 
 // 2. Define the number of layers you want to be available in VIA.
 //    This number MUST match the number of layers in your keymap.c file.
--- keyboards/kayakeeb_v1/rules.mk
+++ keyboards/kayakeeb_v1/rules.mk
@@ -1,8 +1,5 @@
 # rules.mk
 
-# Enable split keyboard functionality
-SPLIT_KEYBOARD = yes
-
 # Set the communication driver to serial
 # Basically whether I am using GPIO pins for serial communication or Serial IO pins.
 # GPIO pins have more flexible usage and is what I used.
@@ -12,3 +9,4 @@ SERIAL_DRIVER = vendor
 
 # enable via remapping
 VIA_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
