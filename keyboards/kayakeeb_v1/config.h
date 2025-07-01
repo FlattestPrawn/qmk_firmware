60458489c8abe457118b1ad736b589102ddfe7d0
Vincent Su <vincent.su@gatech.edu>
2025-07-01 01:31:07 -0700

    switched tx and rx pin just in case

--- keyboards/kayakeeb_v1/config.h
+++ keyboards/kayakeeb_v1/config.h
@@ -9,8 +9,8 @@
 // Define the specific GPIO pins on your RP2040 for communication.
 // The TX of one half must be wired to the RX of the other.
 #define SERIAL_USART_FULL_DUPLEX
-#define SERIAL_USART_TX_PIN GP8
-#define SERIAL_USART_RX_PIN GP9
+#define SERIAL_USART_TX_PIN GP9
+#define SERIAL_USART_RX_PIN GP8
 
 // --- Optional: Increase Communication Speed ---
 // You can optionally increase the serial communication speed for lower latency.
